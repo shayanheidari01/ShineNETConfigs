587e8621fb47d3fb32e1a55d7d209e1f660de264
Shayan Heidari <119259115+shayanheidari01@users.noreply.github.com>
2026-01-07 13:19:44 +0330

    Update v2ray_mining.py

--- v2ray_mining.py
+++ v2ray_mining.py
@@ -85,7 +85,7 @@ def clean_uri(uri: str) -> str:
     uri = uri.strip().rstrip('/')
     if uri.startswith("vmess://"):
         uri = transform_vmess(uri)
-    return uri + "#Telegram: @ShineNETVPN - @ShythonX"
+    return uri + "#Telegram: @ShineNETVPN"
 
 
 # -------- 1) Mining Telegram --------
